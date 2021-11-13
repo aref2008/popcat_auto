51d29bcf0671404c770db4ae33e6ff71045403e7
aref2008 <aref.shaheed.sy@gmail.com>
2021-11-13 17:45:04 +0200

    popclick_auto v-0.1

---
+++ README.md
@@ -0,0 +1,37 @@
+# popcat_auto
+Automate boring clicks on https://popcat.click !! (using Edge, you can change webdriver by replace the current in the folder)
+
+click numbers and wait period randomized to bypass any potential automation detection technique! 
+
+<b>Python:</b><br>
+
+<b>Follow these steps :</b><br>
+1- Download source code.<br>
+2- Install Python 3.9<br>
+3- run "pip install -r req.txt"<br>
+4- run "python popcat.py"<br>
+5- If code not work visit [a link](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/) and download webdriver depend on your Edge version and replace it in folder!<br>
+
+
+
+
+Chrome web driver:<br>
+[a link](https://chromedriver.chromium.org/getting-started)<br>
+
+Firefox web driver:<br>
+[a link](https://github.com/mozilla/geckodriver/releases)<br>
+
+
+
+<b>JavaScript:</b><br>
+paste this code in your browser console :
+<br>
+```js
+var rand_num = Math.random() * (100) + 650;const app=document.querySelector("#app").__vue__;function pop(){var rand_num = Math.random() * (100) + 650;for(var i=0;i<=rand_num;i++)setTimeout(()=>{document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}))},Math.random() * (5) + 5)}document.cookie="country=SY;expires=Sat, 24 FEB 2022 12:00:00 UTC;path=/",document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}));setInterval(()=>{pop()},30000);
+```
+<br>
+
+<b>"How to use it" video on Youtube:</b><br>
+
+
+Enjoy :)
---
+++ popcat.py
@@ -0,0 +1,26 @@
+try:
+	from selenium import webdriver
+except Exception as e:
+	print('run "pip install -r req.txt"')
+
+try:
+	driver = webdriver.Edge("edge.exe")
+	driver.get("https://popcat.click/")
+	driver.execute_script('var rand_num = Math.random() * (100) + 650;const app=document.querySelector("#app").__vue__;function pop(){var rand_num = Math.random() * (100) + 650;for(var i=0;i<=rand_num;i++)setTimeout(()=>{document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}))},Math.random() * (5) + 5)}document.cookie="country=SY;expires=Sat, 24 FEB 2022 12:00:00 UTC;path=/",document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}));setInterval(()=>{pop()},30000);')
+except Exception, e:
+	print('chrome webdriver not valid, please download one that fit with your edge version!!')
+
+try:
+	driver = webdriver.Chrome("chrome.exe")
+	driver.get("https://popcat.click/")
+	driver.execute_script('const rand_num = Math.random() * (100) + 650;const app=document.querySelector("#app").__vue__;function pop(){for(var i=0;i<=rand_num;i++)setTimeout(()=>{document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}))},Math.random() * (5) + 5)}document.cookie="country=SY;expires=Sat, 24 FEB 2022 12:00:00 UTC;path=/",document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}));setInterval(()=>{pop()},30000);')
+except Exception as e:
+	print('chrome webdriver not exist, please download, move it to code folder with name chrome.exe and run code again')
+
+
+try:
+	driver = webdriver.Firefox("firefox.exe")
+	driver.get("https://popcat.click/")
+	driver.execute_script('const rand_num = Math.random() * (100) + 650;const app=document.querySelector("#app").__vue__;function pop(){for(var i=0;i<=rand_num;i++)setTimeout(()=>{document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}))},Math.random() * (5) + 5)}document.cookie="country=SY;expires=Sat, 24 FEB 2022 12:00:00 UTC;path=/",document.dispatchEvent(new KeyboardEvent("keydown",{key:"s",ctrlKey:!0}));setInterval(()=>{pop()},30000);')
+except Exception as e:
+	print('firefox webdriver not exist, please download, move it to code folder with name firefox.exe and run code again')
